66156fd142adb7cb92d76aec2b1c2ba91b3591c3
daniel cheang <daniel@cpttm.org.mo>
2022-05-19 20:14:17 +0800

    set parent and child for item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -87,6 +87,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:name, :cover_image)
+      params.require(:item).permit(:name, :cover_image, :parent_name)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,17 @@ class Item < ApplicationRecord
 	has_many :ingredients, dependent: :nullify
   has_many :recipes, through: :ingredients, source: :recipe
 
+	belongs_to :parent, class_name: "Item", foreign_key: :parent_id, optional: true
+	has_many :children, class_name: "Item", foreign_key: :parent_id
+
   def to_s
     self.name
   end
+
+  def parent_path
+    self.parent ? self.parent.parent_path + [self.parent] : []
+  end
+
 
   before_validation :cleanup
   def cleanup
@@ -24,4 +32,19 @@ class Item < ApplicationRecord
 
     return self.where(["lower(name) like ?", "%#{query.strip.downcase}%"])
   end
+
+  def prent_name
+    self.parent.try :name
+  end
+  attr_accessor :parent_name
+	before_validation :save_parent
+	def save_parent
+		self.parent_name = self.parent_name.to_s.strip.titleize
+    p self.parent_name
+    unless self.parent_name.blank?
+      item = Item.find_or_create_by name: self.parent_name
+      p item
+      self.parent = item
+    end
+	end
 end
